18373aa9ac8b327b05f3ef1eaadbedede2f4a5fe
Dmitry <ldmi3i@gmail.com>
2022-07-08 13:35:52 +0300

    feat: add base structure

---
+++ internal/process/processor.go
@@ -0,0 +1,55 @@
+package process
+
+import (
+	"context"
+	"github.com/rog-golang-buddies/internal/load"
+	"github.com/rog-golang-buddies/internal/model"
+	"github.com/rog-golang-buddies/internal/parse"
+	"github.com/rog-golang-buddies/internal/recognize"
+)
+
+//UrlProcessor represents provide entrypoint for the url processing
+//full processing of the incoming
+type UrlProcessor interface {
+	process(ctx context.Context, url string) (*model.ApiSpecDoc, error)
+}
+
+type ProcessorImpl struct {
+	recognizer    recognize.Recognizer
+	converter     parse.Converter
+	contentLoader load.ContentLoader
+}
+
+func (p *ProcessorImpl) process(ctx context.Context, url string) (*model.ApiSpecDoc, error) {
+	//Check availability of url
+	//...
+
+	//Load content by url
+	file, err := p.contentLoader.Load(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+
+	//If no errs recognize file type by content
+	fileType, err := p.recognizer.RecognizeFileType(file)
+	if err != nil {
+		return nil, err
+	}
+
+	//Parse API spec of defined type
+	apiSpec, err := p.converter.Convert(file.Content, fileType)
+	if err != nil {
+		return nil, err
+	}
+
+	return apiSpec, nil
+}
+
+func NewProcessor() (UrlProcessor, error) {
+	//Need to pass dependencies through constructor
+	return &ProcessorImpl{
+		recognizer:    recognize.NewRecognizer(),
+		converter:     parse.NewConverter([]parse.Parser{parse.NewYamlOpenApiParser(), parse.NewJsonOpenApiParser()}),
+		contentLoader: load.NewContentLoader(),
+	}, nil
+}
